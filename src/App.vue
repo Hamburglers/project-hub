07e3776724cfe70a9b6bb4da006461f1299c12ab
allen <allenwlfang@gmail.com>
2023-12-08 17:42:21 +1100

    Added dividers between sections

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,13 @@ const currentView = computed(() => {
         </div>
       </section>
       <section class="tres">
+        <div class="custom-shape-divider-top-1702017514">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+                <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+                <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+            </svg>
+        </div>
         <div class="trestext">
         <a href="#/todo"><img src="/blank.png"></a>
         <a href="#/todo" id="button">Coming soon</a>
@@ -97,6 +104,27 @@ body {
     fill: #060B3E;
 }
 
+.custom-shape-divider-top-1702017514 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+}
+
+.custom-shape-divider-top-1702017514 svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: 190px;
+}
+
+.custom-shape-divider-top-1702017514 .shape-fill {
+    fill: #FFFFFF;
+}
+COPY HTMLCOPY CSSDOWNLOAD SVG
+
 header, h2{
   display: flex;
   justify-content: center;
@@ -139,7 +167,7 @@ a {
   height: 20%;
   gap: 10px;
   justify-content: center;
-  font-size: 25px;
+  font-size: 2vw;
   border-radius: 5px;
   color: white;
   margin-top: 10px;
@@ -151,7 +179,7 @@ a:hover {
   cursor: pointer;
   background-color: white;
   color: black;
-  scale: 1.08;
+  scale: 1.05;
 }
 
 #button {
@@ -165,7 +193,7 @@ a:hover {
 }
 
 .inverse:hover {
-  scale: 1.08;
+  scale: 1.05;
   color: white;
   background-color: #060b3e;
   cursor: pointer;
@@ -200,16 +228,20 @@ img {
   display: flex;
   flex-direction: column;
   width: 100%; /* Full width */
-  padding: 40px;
+  padding: 60px;
   transition: transform 0.5s; /* Smooth transition for the transform */
 }
 
 .unotext, .dostext, .trestext {
   max-width: 35vw;
-  font-size: 15px;
+  font-size: 1vw;
   text-align: center;
   margin: 0 auto;
   color: white;
+}
+
+.dostext {
+  color: black;
 }
 
 /* Apply transform to shift each section */
