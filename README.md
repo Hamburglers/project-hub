18f209107e9a629c065db6082e19e20f1414721b
allen <allenwlfang@gmail.com>
2023-12-08 13:19:11 +1100

    Basic outline for hub

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # project-hub
 A hub that connects all projects
+
+Todo list:
+    fix list items jumping possibly id issue
--- src/App.vue
+++ src/App.vue
@@ -21,16 +21,25 @@ const currentView = computed(() => {
 <template>
   <div v-if="!currentView">
     <header>A collection of projects by Allen</header>
-    <div class="linkContainer">
+    <section class="linkContainer">
+      <section class="uno">
+        <div class="unotext">
         <a href="#/todo"><img src="/todolist.png">To do list</a>
+        <br>
+        Connected to PostgreSQL databse with Websocket and 
+          fallback HTTP requests for real time synchronisation.</div>
+      </section>
+      <section class="dos">
+        <div class="dostext">
         <a href="#/todo"><img src="/blank.png">Coming soon</a>
+        </div>
+      </section>
+      <section class="tres">
+        <div class="trestext">
         <a href="#/todo"><img src="/blank.png">Coming soon</a>
-        <a href="#/todo"><img src="/blank.png">Coming soon</a>
-        <a href="#/todo"><img src="/blank.png">Coming soon</a>
-        <a href="#/todo"><img src="/blank.png">Coming soon</a>
-        <a href="#/todo"><img src="/blank.png">Coming soon</a>
-        <a href="#/todo"><img src="/blank.png">Coming soon</a>
-    </div>
+        </div>
+      </section>
+    </section>
   </div>
   <component :is="currentView" />
 </template>
@@ -46,25 +55,36 @@ body {
   min-width: none;
   min-height: none;
   margin: 0px;
+  overflow-x: hidden;
 }
 </style>
 
 <style scoped>
+
 header {
   text-align: center;
   align-items: center;
-  margin-top: 0px;
-  margin-bottom: 30px;
-  padding: 20px 0;
-  top: 0px;
-  left: 0px;
-  right: 0px;
+  margin: 0;
+  padding: 20px 0 30px 0px;
   width: 100%;
+  background-color: rgb(175, 175, 175);
+}
+
+section {
+  border: 0;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  position: relative;
 }
 
 a {
   text-align: center;
-  width: 15vw;
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 35vw;
+  gap: 10px;
+  justify-content: center;
 }
 
 a:hover {
@@ -76,19 +96,56 @@ a:hover {
 } */
 .linkContainer {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   flex-wrap: wrap;
-  max-width: 80vw;
   justify-content: center;
   align-items: center;
-  gap: 30px;
+  padding: 40px;
+  width: 100vw;
+  margin-top: -40px;
 }
 
 img {
-  width: 15vw;
-  height: 15vh;
+  max-width: 50vw;
+  height: auto;
   left: 0px;
   border: 2px solid black;
   border-radius: 15px;
+}
+
+.unotext {
+  max-width: 35vw;
+  font-size: 15px;
+  padding: 40px 15vw;
+}
+
+.dostext {
+  max-width: 35vw;
+  font-size: 15px;
+  padding: 40px 65vw;
+}
+
+.trestext {
+  max-width: 35vw;
+  font-size: 15px;
+  padding: 40px 50vw;
+}
+
+.uno {
+  display: flex;
+  flex-direction: column;
+  background-color: rgb(175, 175, 175);
+  padding: 40px;
+  border: 0px;
+}
+
+.dos {
+  background-color: rgb(203, 203, 203);
+  padding: 40px;
+}
+
+.tres {
+  background-color: rgb(255, 255, 255);
+  padding: 40px;
 }
 </style>
